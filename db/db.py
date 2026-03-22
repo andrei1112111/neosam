57c93622ca68fbd261bc72e9763b5ff64aecce91
andrei1112111 <atishkin060705@gmail.com>
2026-03-22 20:49:24 +0700

    обработка абсолютного пути к фалу базы

--- db/db.py
+++ db/db.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 from peewee import SqliteDatabase
 
 
-db = SqliteDatabase('my_database.db')
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+DB_PATH = PROJECT_ROOT / "my_database.db"
 
+db = SqliteDatabase(DB_PATH)
